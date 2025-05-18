09871f7c4d151d4a0740796fa61f6454d158c24f
Carlos Padilla <carlos.opg.16@gmail.com>
2025-05-18 14:32:39 -0700

    Se realizacion cambios para añadir formularios, de registro de pacientes, registro de doctores, agenda de citas, registro de consultorios.

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4f5c0a3c9e3084978d9f073c2b9858915e315e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b328a284abb91e3d6c9e6ebc9d763ca030f6d2a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
